6bb1e2c9a14d174a41abada195fd79f8bacd718a
Michael Krohn <michaelkrohn@MacBook-Pro-8.localdomain>
2019-02-27 16:23:25 -0500

    compiles

--- src/main/java/com/zipcodewilmington/assessment2/part3/SpeedComparator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/SpeedComparator.java
@@ -1,6 +1,4 @@
 package com.zipcodewilmington.assessment2.part3;
 
-import org.h2.mvstore.db.MVSecondaryIndex;
-
-public class SpeedComparator implements MVSecondaryIndex.Source.Comparator {
+public class SpeedComparator {
 }
